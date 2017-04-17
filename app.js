83c9df58d8905f15a9a05a96ff18071640709e75
Veronica  Mesuraca <veronicamesuraca@Veronicas-Air.station>
2017-04-17 12:12:14 +0200

    Add tabs and tab component but still cannot add symbol as class

--- app.js
+++ app.js
@@ -1,4 +1,53 @@
+Vue.component('tabs', {
+	template: `
+		<div class="container">
+			<div class="tabs is-centered is-boxed is-medium">
+				  <ul>
+					  <li v-for="tab in tabs">
+					    <a>
+								<i :class="{{tab.symbol-class}}"></i>
+								<span>{{tab.name}}</span>
+					  	</a>
+					  
+					  </li>
+				    
+				  </ul>
+				</div>
+				
+				<div class="tabs-details">
+					<slot></slot>
+				</div>
+		</div>
+	`,
 
+props: {
+	class: {}
+},
+
+data() {
+	return {
+		tabs: []
+
+	};
+},
+
+	created() {
+		this.tabs = this.$children;
+	},
+
+});
+
+
+Vue.component('tab', {
+	template: `
+		<div><slot></slot></div>
+	`,
+	props: {
+		name: {required: true},
+		symbolClass: {required: true}
+	},
+
+});
 
 
 
